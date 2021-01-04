1dad2a8cadddb496b18c464691753b10ad11c66e
Shana Wiltshire <shanawiltshire@gmail.com>
2021-01-04 01:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var kittens = ["Milo", "Otis", "Garfield"]
-
-function destructivelyAppendKitten(name){
-    return kittens.append("Odie")
-};
+ // First, we're going to create four arrays of pets. Define 4 `const` called:
+//  `append`, `prepend`, `removeLast` and `removeFirst`, and set all to an initial
+//  value of `["Milo", "Otis", "Garfield"]`. These are the `array`s that we'll work
+//  with for each exercise.
