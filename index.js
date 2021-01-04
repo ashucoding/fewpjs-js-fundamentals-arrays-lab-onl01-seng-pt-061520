9e050d1c8882f87300a5714de26e39b71764d2f8
Shana Wiltshire <shanawiltshire@gmail.com>
2021-01-04 01:14:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const append = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
 
 const prepend = ["Milo", "Otis", "Garfield"];
+prepend.pop("Odie");
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
